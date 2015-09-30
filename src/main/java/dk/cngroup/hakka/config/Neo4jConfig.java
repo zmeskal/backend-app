1924e4bfed0ac90dd492bf8766e5011c50f0af4b
Bob Stalmach <bob.stalmach@gmail.com>
2015-10-01 01:22:39 +0200

    Make DB URL settable from env. variable
    Seed service removed as well.

--- src/main/java/dk/cngroup/hakka/config/Neo4jConfig.java
+++ src/main/java/dk/cngroup/hakka/config/Neo4jConfig.java
@@ -1,12 +1,9 @@
 package dk.cngroup.hakka.config;
 
-import dk.cngroup.hakka.service.SeedService;
 import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.server.Neo4jServer;
@@ -14,21 +11,18 @@ import org.springframework.data.neo4j.server.RemoteServer;
 
 @Configuration
 @EnableNeo4jRepositories(Neo4jConfig.REPOSITORIES)
-public class Neo4jConfig extends Neo4jConfiguration implements CommandLineRunner {
+public class Neo4jConfig extends Neo4jConfiguration {
 
     public static final String REPOSITORIES = "dk.cngroup.hakka.repository";
     public static final String ENTITIES = "dk.cngroup.hakka.entity";
 
-    @Autowired
-    private Environment environment;
-
-    @Autowired
-    private SeedService seedService;
+    @Value("${neo4j.database.url:'localhost:7474'}")
+    String databaseUrl;
 
     @Override
     @Bean
     public Neo4jServer neo4jServer() {
-        return new RemoteServer(environment.getProperty("neo4j.database.url"));
+        return new RemoteServer(databaseUrl);
     }
 
     @Override
@@ -37,9 +31,4 @@ public class Neo4jConfig extends Neo4jConfiguration implements CommandLineRunner
         return new SessionFactory(Neo4jConfig.ENTITIES);
     }
 
-
-    @Override
-    public void run(String... strings) throws Exception {
-        seedService.seedData();
-    }
 }

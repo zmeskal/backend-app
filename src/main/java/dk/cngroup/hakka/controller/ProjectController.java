24ecb752a611ee145f5d5ef6ef53c92e9ea917f3
Boris Musatov <boris.musatov@gmail.com>
2015-12-10 18:24:23 +0100

    add spring data rest and logging

--- src/main/java/dk/cngroup/hakka/controller/ProjectController.java
+++ src/main/java/dk/cngroup/hakka/controller/ProjectController.java
@@ -3,35 +3,36 @@ package dk.cngroup.hakka.controller;
 import dk.cngroup.hakka.controller.routes.Routes;
 import dk.cngroup.hakka.entity.Project;
 import dk.cngroup.hakka.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.ws.rs.core.MediaType;
-import java.util.List;
+import org.springframework.data.rest.core.config.Projection;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = Routes.PROJECTS, produces = MediaType.APPLICATION_JSON)
+@RequestMapping(value = Routes.PROJECTS, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ProjectController {
 
     private final ProjectRepository projectRepository;
-
-    @Autowired
-    public ProjectController(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
-    }
 
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Project> list() {
         return projectRepository.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Project create(@RequestBody Project project) {
         return projectRepository.save(project);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Project findOne(@PathVariable("id") Long id) {
+        return projectRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id) {
+        projectRepository.delete(id);
     }
 }
